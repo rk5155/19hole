923106c0c6bccb7d1108cba1ea65f9c25c8fe2d3
rk <>
2021-10-22 17:23:08 +0900

    homeにコンポーネントを使用し、リストを作成した

--- home.vue
+++ home.vue
@@ -1,14 +1,39 @@
 <template>
-  <div>
-      <b>{{ text }}</b>
-  </div>
+    <div>
+        <b>{{ text }}</b>
+            
+        <div id="member" class="container">
+            <div class="row">
+                
+
+
+
+                <member title="title1"></member>
+                <member title="title2"></member>
+                <member title="title3"></member>
+
+                
+                
+            </div>
+            
+        </div>
+
+        
+    </div>
 </template>
+
 <script>
-module.exports = {
-  data: function() {
-    return {
-      text: 'home'
+    module.exports = {
+        data: function() {
+            return {
+            text: 'home'
+            }
+        }
     }
-  }
-}
+
+    Vue.component('member', {
+        props: ['title'],
+        template: '<div id="member" class="col-md-4"><h2>{{ title }}</h2></div>'
+    })
+
 </script>
