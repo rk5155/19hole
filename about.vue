9fcc7ecf0226ff5e5dfd3cf4e7e53062aab97f42
rk <>
2021-10-29 16:56:06 +0900

    home のメインテキストをコンポーネント化し他ページでも使用できるようにした

--- about.vue
+++ about.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
       <b>{{ text }}</b>
+
+      <main-text v-for="text in mainTexts" v-bind:key="text.id" v-bind:text="text"></main-text>
       
   </div>
 </template>
@@ -9,7 +11,10 @@
 module.exports = {
   data: function() {
     return {
-      text: 'about'
+      text: 'about',
+      mainTexts: [
+                    { id: 1, path: "img/img_01.jpg", h2: 'ゴルフで人と繋がる。', p: `ゴルフを始めたけど、一緒に行く仲間がいない。<br>たくさんの人に出会えるし、少しでも上手くなろうと一生懸命に練習する姿は美しい。<br>同世代のゴルフ友達が欲しい。<br>ゴルフを通してたくさんの知り合いが欲しい。<br>Enjoy Golf!! 新しいゴルフが始まります。` }
+                ]
     }
   }
 }
--- js/sample.js
+++ js/sample.js
@@ -28,3 +28,17 @@ new Vue({
       Slide: window.Hooper.Slide
   }
 })
+
+
+// 子コンポーネント
+Vue.component('main-text', {
+  props: ['text'],
+  template: `<div class="text-box">
+                <div>
+                    <h2>{{ text.h2 }}</h2>
+                    <p v-html="text.p"></p>
+                </div>
+
+                <img v-bind:src="text.path">
+              </div>`
+})
