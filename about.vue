71d2af31b842b754c3bcc1c666a73e6095add8d1
rk <>
2021-11-05 16:22:58 +0900

    about テキスト修正

--- about.vue
+++ about.vue
@@ -2,10 +2,6 @@
   <div id="about">
 
       <main-text v-for="text in mainTexts" v-bind:key="text.id" v-bind:text="text"></main-text>
-
-      <main-text v-for="text in mainTexts" v-bind:key="text.id" v-bind:text="text"></main-text>
-
-      <main-text v-for="text in mainTexts" v-bind:key="text.id" v-bind:text="text"></main-text>
       
   </div>
 </template>
@@ -16,7 +12,8 @@ module.exports = {
     return {
       text: 'about',
       mainTexts: [
-                    { id: 1, path: "img/img_01.jpg", h2: 'ゴルフで人と繋がる。', p: `ゴルフを始めたけど、一緒に行く仲間がいない。<br>たくさんの人に出会えるし、少しでも上手くなろうと一生懸命に練習する姿は美しい。<br>同世代のゴルフ友達が欲しい。<br>ゴルフを通してたくさんの知り合いが欲しい。<br>Enjoy Golf!! 新しいゴルフが始まります。` }
+                    { id: 1, path: "img/img_01.jpg", h2: '20,30代の関東ゴルフサークル', p: `メンバー随時募集しています⛳️<br>同年代で初心者、経験者問わず、誰でもゴルフが好きな人が楽しめるサークルです。飲み会、お花見、BBQなども開催しています。` },
+                    { id: 1, path: "img/img_01.jpg", h2: '活動内容', p: `コンペ、ラウンド交流会、シミュレーションゴルフや練習会・飲み会、お花見、BBQなど` }
                 ]
     }
   }
